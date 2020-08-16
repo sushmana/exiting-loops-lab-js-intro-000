457d6b1e24fa59a116f840b87cd7c24419630a53
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-16 20:37:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function breakOut(array,changevalue,stopValue){
+function breakOut(array,changeValue,stopValue){
   for(let i=0;i<array.length;i++){
     if(array[i]!==stopValue){
       array[i]=changeValue;
